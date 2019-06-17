4eae8ba139e1cfc20eff9a72b18401a04a219fd1
dushansilva <dushansilva95@gmail.com>
2019-06-17 19:55:14 +0530

    fix formatting issues

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/ldap/LDAPConnectionContext.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/ldap/LDAPConnectionContext.java
@@ -207,14 +207,14 @@ public class LDAPConnectionContext {
         //if dcMap is not populated, it is not DNS case
         if (dcMap == null) {
             try {
-                context = getLdapContext(environment,null);
+                context = getLdapContext(environment, null);
 
             } catch (NamingException e) {
                 log.error("Error obtaining connection. " + e.getMessage(), e);
                 log.error("Trying again to get connection.");
 
                 try {
-                    context = getLdapContext(environment,null);
+                    context = getLdapContext(environment, null);
                 } catch (Exception e1) {
                     log.error("Error obtaining connection for the second time" + e.getMessage(), e);
                     throw new UserStoreException("Error obtaining connection. " + e.getMessage(), e);
@@ -228,7 +228,7 @@ public class LDAPConnectionContext {
                 SRVRecord firstRecord = dcMap.get(firstKey);
                 //compose the connection URL
                 environment.put(Context.PROVIDER_URL, getLDAPURLFromSRVRecord(firstRecord));
-                context = getLdapContext(environment,null);
+                context = getLdapContext(environment, null);
 
             } catch (NamingException e) {
                 log.error("Error obtaining connection to first Domain Controller." + e.getMessage(), e);
@@ -238,7 +238,7 @@ public class LDAPConnectionContext {
                     try {
                         SRVRecord srv = dcMap.get(integer);
                         environment.put(Context.PROVIDER_URL, getLDAPURLFromSRVRecord(srv));
-                        context = getLdapContext(environment,null);
+                        context = getLdapContext(environment, null);
                         break;
                     } catch (NamingException e1) {
                         if (integer == (dcMap.lastKey())) {
