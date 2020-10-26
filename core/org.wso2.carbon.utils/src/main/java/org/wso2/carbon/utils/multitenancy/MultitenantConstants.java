2dfa148cfed61b832a6be1547bf2db17056e88ba
erandipraboda <erandipraboda@gmail.com>
2020-10-26 13:09:02 +0530

    Removing formatting changes

--- core/org.wso2.carbon.utils/src/main/java/org/wso2/carbon/utils/multitenancy/MultitenantConstants.java
+++ core/org.wso2.carbon.utils/src/main/java/org/wso2/carbon/utils/multitenancy/MultitenantConstants.java
@@ -1,17 +1,17 @@
-/*
- * Copyright 2004,2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/*                                                                             
+ * Copyright 2004,2005 The Apache Software Foundation.                         
+ *                                                                             
+ * Licensed under the Apache License, Version 2.0 (the "License");             
+ * you may not use this file except in compliance with the License.            
+ * You may obtain a copy of the License at                                     
+ *                                                                             
+ *      http://www.apache.org/licenses/LICENSE-2.0                             
+ *                                                                             
+ * Unless required by applicable law or agreed to in writing, software         
+ * distributed under the License is distributed on an "AS IS" BASIS,           
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.    
+ * See the License for the specific language governing permissions and         
+ * limitations under the License.                                              
  */
 package org.wso2.carbon.utils.multitenancy;
 
@@ -63,18 +63,18 @@ public class MultitenantConstants {
     public static final String TENANT_REQUEST_MSG_CTX = "TENANT_REQUEST_MSG_CTX";
 
     public static final String SSO_AUTH_SESSION_ID = "SSOAuthSessionID";
-
+    
     public static final String HTTP_SC ="HTTP_SC";
-
+    
     public static final String PASS_THROUGH_PIPE = "pass-through.pipe";
-
+    
     public static final String MESSAGE_BUILDER_INVOKED = "message.builder.invoked";
-
+    
     public static final String PASS_THROUGH_SOURCE_CONFIGURATION =
         "PASS_THROUGH_SOURCE_CONFIGURATION";
-
+    
     public static final String PASS_THROUGH_SOURCE_CONNECTION = "pass-through.Source-Connection";
-
+    
     public static final String CONTENT_TYPE = "ContentType";
 
     public static final String USER_INFO_HANDLER = "UserInfoHandler";
@@ -90,7 +90,7 @@ public class MultitenantConstants {
     public static final String FORCE_HTTP_CONTENT_LENGTH = "FORCE_HTTP_CONTENT_LENGTH";
     /**
      * Properties required for evaluating POST_TO_URI
-     */
+     */    
     public static final String POST_TO_URI = "POST_TO_URI";
     public static final String COPY_CONTENT_LENGTH_FROM_INCOMING = "COPY_CONTENT_LENGTH_FROM_INCOMING";
     public final static String PASSTROUGH_MESSAGE_LENGTH = "PASSTROUGH_MESSAGE_LENGTH";
